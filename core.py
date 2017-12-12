fa74879b9734f91eb5d4bcb900dcf5e15c17abab
Atk <darkninjabuni@gmail.com>
2017-12-12 14:56:22 +0000

    OOP & quit is handled more cleanly

--- core.py
+++ core.py
@@ -20,7 +20,7 @@ class Player():
     """
     This is the player's actor
     """
-    def __init__(self, x = 10, y = 10, w = 15, h = 15):
+    def __init__(self, x = 10, y = 10, w = 15, h = 15, colour = red):
         """
         Parameters provided allow for a better customisation of the player "model"
         """
@@ -28,6 +28,7 @@ class Player():
         self.y = y
         self.w = w
         self.h = h
+        self.colour = colour
 
     def __str__(self):
         """
@@ -43,8 +44,8 @@ class Player():
         """
         return "Player is at ({x},{y}), and is {w} x {h} pixels.".format(x = self.x, y = self.y, w = self.w, h = self.h)
 
-def player(colour, x, y, w, h):
-    pygame.draw.rect(gameDisplay, colour, [x, y, w, h])
+    def draw(self):
+        pygame.draw.rect(gameDisplay, self.colour, [self.x, self.y, self.w, self.h])
 
 def drawCrosshair(mouse, player, colour=black):
     # top hair
@@ -55,12 +56,12 @@ def drawCrosshair(mouse, player, colour=black):
     pygame.draw.rect(gameDisplay, colour, [mouse[0] - 10, mouse[1] - 1, 6, 2])
     # right hair
     pygame.draw.rect(gameDisplay, colour, [mouse[0] + 4, mouse[1] - 1, 6, 2])
-    pygame.draw.lines(gameDisplay, colour, mouse, player, 2)
+    #pygame.draw.lines(gameDisplay, colour, mouse, player, 2)
 
 def instance():
     running = True
-    playerx = 10
-    playery = 10
+
+    player = Player()
 
     # hide mouse
     pygame.mouse.set_visible(False)
@@ -73,7 +74,7 @@ def instance():
 
             # Quit the game
             if event.type == pygame.QUIT:
-                pygame.quit()
+                running = False
 
             # Key pressed (triggers once, even if held) #
             if event.type == pygame.KEYDOWN:
@@ -85,19 +86,19 @@ def instance():
 
         # W
         if keys[pygame.K_w]:
-            playery -= 1
+            player.y -= 1
             print("up")
         # A
         if keys[pygame.K_a]:
-            playerx -= 1
+            player.x -= 1
             print("left")
         # S
         if keys[pygame.K_s]:
-            playery += 1
+            player.y += 1
             print("down")
         # D
         if keys[pygame.K_d]:
-            playerx += 1
+            player.x += 1
             print("right")
         ###
 
@@ -106,11 +107,14 @@ def instance():
 
         # Draw things to screen #
         gameDisplay.fill(white)
-        player(red, playerx, playery, 15, 15)
-        drawCrosshair(mousePos, (playerx, playery), black)
+        #player(red, player.x, player.y, 15, 15)
+        player.draw()
+        drawCrosshair(mousePos, (player.x, player.y), black)
 
         ###
         pygame.display.update()
         clock.tick(120)
+
+    pygame.quit()
 
 instance()
