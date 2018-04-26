9b0fbe0c7d4893dea540f989ba023f714de56db0
AtkLordOverAll <darkninjabuni@gmail.com>
2018-04-26 18:06:40 +0100

    stress test suite

---
+++ stresstestcreator.py
@@ -0,0 +1,7 @@
+file = open("-200.level", "w+")
+file.write("10 10\n500 500 100-600-1080-600-500-20 1.2\n")
+for n in range(1280):
+    for m in range(720):
+        file.write("{nn} {mm} 1 1 False ".format(nn = n, mm = m))
+file.close()
+print("Done")
