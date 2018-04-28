33e10a875e29a2b4e58d7197ea5a35b077c149bb
AtkLordOverAll <darkninjabuni@gmail.com>
2018-04-28 17:00:27 +0100

    saves level in correct place, talks more

--- stresstestcreator.py
+++ stresstestcreator.py
@@ -1,5 +1,6 @@
-file = open("-200.level", "w+")
+file = open("./levels/-200.level", "w+")
 file.write("10 10\n500 500 100-600-1080-600-500-20 1.2\n")
+print("File initialised")
 for n in range(1280):
     for m in range(720):
         file.write("{nn} {mm} 1 1 False ".format(nn = n, mm = m))
